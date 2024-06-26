718cad57ef059f72e671ec4415bdbb9962a739fa
Sam Schmitt <sam@samschmitt.com>
2024-06-25 21:30:28 -0400

    Add basic net/http setup

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
-import "net/http"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"log"
+)
 
 func main() {
 	fmt.Println("ehhl")
+
+	handler := func(w http.ResponseWriter, req *http.request) {
+		io.WriteString(w, "ehhl\n")
+	}
+
+	http.HandleFunc("/hello", handler)
+	log.Fatal(http.ListenAndServe(":8888", nil))
 }
